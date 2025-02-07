80271414a7a8ae1e4794a2b6e57a913107186ddc
Ahin K <ahin.khan@cypoint.se>
2025-02-07 14:54:22 +0100

    updated flask with logo

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM python:3.9-slim
+
 WORKDIR /app
 COPY app.py /app/
+COPY templates/ /app/templates/
+COPY static/ /app/static/
+
 RUN pip install flask
 ENV FLASK_APP=app.py
+
 CMD ["flask", "run", "--host=0.0.0.0", "--port=5000"]
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     pod_name = os.getenv("POD_NAME", "Unknown Pod")
-    return f"<h1>Version 2: Served from Pod: {pod_name}</h1>"
+    return render_template('index.html', pod_name=pod_name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
